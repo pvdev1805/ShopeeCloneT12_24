536e770f9dd246ebdac992988bda773cde08ec9c
Phu Vo <u1153426@umail.usq.edu.au>
2025-01-01 06:12:48 +1000

    Implement logic for SortProductList component

--- src/pages/ProductList/SortProductList/SortProductList.tsx
+++ src/pages/ProductList/SortProductList/SortProductList.tsx
@@ -1,60 +1,186 @@
-const SortProductList = () => {
+import classNames from 'classnames'
+import { sortBy, order as OrderConstant } from '../../../constants/product'
+import { ProductListConfig } from '../../../types/product.type'
+import { QueryConfig } from '../ProductList'
+import { createSearchParams, Link, useNavigate } from 'react-router-dom'
+import path from '../../../constants/path'
+import { omit } from 'lodash'
+
+interface Props {
+  queryConfig: QueryConfig
+  pageSize: number
+}
+
+const SortProductList = ({ queryConfig, pageSize }: Props) => {
+  const page = Number(queryConfig.page)
+
+  const { sort_by = sortBy.createdAt, order } = queryConfig
+
+  const navigate = useNavigate()
+
+  const isActiveSortBy = (sortByValue: Exclude<ProductListConfig['sort_by'], undefined>) => {
+    return sort_by === sortByValue
+  }
+
+  const handleSort = (sortByValue: Exclude<ProductListConfig['sort_by'], undefined>) => {
+    navigate({
+      pathname: path.home,
+      search: createSearchParams(
+        omit(
+          {
+            ...queryConfig,
+            sort_by: sortByValue
+          },
+          ['order']
+        )
+      ).toString()
+    })
+  }
+
+  const handlePriceOrder = (orderValue: Exclude<ProductListConfig['order'], undefined>) => {
+    navigate({
+      pathname: path.home,
+      search: createSearchParams({
+        ...queryConfig,
+        sort_by: sortBy.price,
+        order: orderValue
+      }).toString()
+    })
+  }
+
   return (
     <>
       <div className='bg-gray-300/40 py-4 px-3'>
         <div className='flex flex-wrap items-center justify-between gap-2'>
           <div className='flex items-center flex-wrap gap-2'>
             <div>Sort By</div>
-            <button className='h-8 px-4 capitalize bg-orange text-white text-sm hover:bg-orange/80 text-center'>
+            <button
+              className={classNames('h-8 px-4 capitalize text-sm text-center', {
+                'bg-orange text-white hover:bg-orange/80': isActiveSortBy(sortBy.view),
+                'bg-white text-black hover:bg-slate-100': !isActiveSortBy(sortBy.view)
+              })}
+              onClick={() => handleSort(sortBy.view)}
+            >
               Popularity
             </button>
-            <button className='h-8 px-4 capitalize bg-white text-black text-sm hover:bg-slate-100 text-center'>
+            <button
+              className={classNames('h-8 px-4 capitalize text-sm text-center', {
+                'bg-orange text-white hover:bg-orange/80': isActiveSortBy(sortBy.createdAt),
+                'bg-white text-black hover:bg-slate-100': !isActiveSortBy(sortBy.createdAt)
+              })}
+              onClick={() => handleSort(sortBy.createdAt)}
+            >
               New Arrival
             </button>
-            <button className='h-8 px-4 capitalize bg-white text-black text-sm hover:bg-slate-100 text-center'>
+            <button
+              className={classNames('h-8 px-4 capitalize text-sm text-center', {
+                'bg-orange text-white hover:bg-orange/80': isActiveSortBy(sortBy.sold),
+                'bg-white text-black hover:bg-slate-100': !isActiveSortBy(sortBy.sold)
+              })}
+              onClick={() => handleSort(sortBy.sold)}
+            >
               Best Seller
             </button>
             <select
-              className='h-8 px-4 capitalize bg-white text-black text-sm hover:bg-slate-100 text-left outline-none'
-              defaultValue=''
+              className={classNames('h-8 px-4 capitalize text-sm text-left outline-none', {
+                'bg-orange text-white hover:bg-orange/80': isActiveSortBy(sortBy.price),
+                'bg-white text-black hover:bg-slate-100': !isActiveSortBy(sortBy.price)
+              })}
+              value={order || ''}
+              onChange={(event) =>
+                handlePriceOrder(event.target.value as Exclude<ProductListConfig['order'], undefined>)
+              }
             >
-              <option value='' disabled>
+              <option value='' disabled className='bg-white text-black'>
                 Price
               </option>
-              <option value='price:asc'>Price: Ascending Order</option>
-              <option value='price:desc'>Price: Descending Order</option>
+              <option value={OrderConstant.asc} className='bg-white text-black'>
+                Price: Ascending Order
+              </option>
+              <option value={OrderConstant.desc} className='bg-white text-black'>
+                Price: Descending Order
+              </option>
             </select>
           </div>
           <div className='flex items-center'>
             <div>
-              <span className='text-orange'>1</span>
-              <span>/2</span>
+              <span className='text-orange'>{page}</span>
+              <span>/{pageSize}</span>
             </div>
-            <div className='ml-2'>
-              <button className='px-3 h-8 rounded-tl-sm rounded-bl-sm bg-white/60 hover:bg-slate-100 cursor-not-allowed shadow'>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth={1.5}
-                  stroke='currentColor'
-                  className='size-3'
+            <div className='ml-2 flex'>
+              {page === 1 ? (
+                <span className='flex h-8 w-9 cursor-not-allowed items-center justify-center rounded-tl-sm rounded-bl-sm bg-white/60 shadow hover:bg-slate-100'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='size-3'
+                  >
+                    <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
+                  </svg>
+                </span>
+              ) : (
+                <Link
+                  to={{
+                    pathname: path.home,
+                    search: createSearchParams({
+                      ...queryConfig,
+                      page: (page - 1).toString()
+                    }).toString()
+                  }}
+                  className='flex h-8 w-9 items-center justify-center rounded-tl-sm rounded-bl-sm bg-white shadow hover:bg-slate-100'
                 >
-                  <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
-                </svg>
-              </button>
-              <button className='shadow px-3 h-8 rounded-tr-sm rounded-br-sm bg-white hover:bg-slate-100 '>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  strokeWidth={1.5}
-                  stroke='currentColor'
-                  className='size-3'
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='size-3'
+                  >
+                    <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
+                  </svg>
+                </Link>
+              )}
+
+              {page === pageSize ? (
+                <span className='flex h-8 w-9 cursor-not-allowed items-center justify-center rounded-tl-sm rounded-bl-sm bg-white/60 shadow hover:bg-slate-100'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='size-3'
+                  >
+                    <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
+                  </svg>
+                </span>
+              ) : (
+                <Link
+                  to={{
+                    pathname: path.home,
+                    search: createSearchParams({
+                      ...queryConfig,
+                      page: (page + 1).toString()
+                    }).toString()
+                  }}
+                  className='flex h-8 w-9 items-center justify-center rounded-tl-sm rounded-bl-sm bg-white shadow hover:bg-slate-100'
                 >
-                  <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
-                </svg>
-              </button>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth={1.5}
+                    stroke='currentColor'
+                    className='size-3'
+                  >
+                    <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
+                  </svg>
+                </Link>
+              )}
             </div>
           </div>
         </div>
