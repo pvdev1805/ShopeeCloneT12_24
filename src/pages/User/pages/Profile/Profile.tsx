84ae42e8cf422b34bce9b7eec6c8ae8f6983b4af
Phu Vo <u1153426@umail.usq.edu.au>
2025-01-12 19:09:33 +1000

    Display profile data on form

--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -1,6 +1,65 @@
+import { useMutation, useQuery } from '@tanstack/react-query'
+import Button from '../../../../components/Button'
 import Input from '../../../../components/Input'
+import userApi from '../../../../apis/user.api'
+import { userSchema, UserSchema } from '../../../../utils/rules'
+import { Controller, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import InputNumber from '../../../../components/InputNumber'
+import { useEffect } from 'react'
+import DateSelect from '../../components/DateSelect'
+
+type FormData = Pick<UserSchema, 'name' | 'address' | 'phone' | 'avatar' | 'date_of_birth'>
+
+const profileSchema = userSchema.pick(['name', 'address', 'phone', 'avatar', 'date_of_birth'])
 
 const Profile = () => {
+  const { data: profileData } = useQuery({
+    queryKey: ['profile'],
+    queryFn: userApi.getProfile
+  })
+
+  const profile = profileData?.data.data
+
+  const updateProfileMutation = useMutation(userApi.updateProfile)
+
+  const {
+    register,
+    control,
+    formState: { errors },
+    handleSubmit,
+    setValue,
+    watch,
+    setError
+  } = useForm<FormData>({
+    defaultValues: {
+      name: '',
+      address: '',
+      phone: '',
+      avatar: '',
+      date_of_birth: new Date(1990, 0, 1)
+    },
+    resolver: yupResolver(profileSchema)
+  })
+
+  useEffect(() => {
+    if (profile) {
+      setValue('name', profile.name)
+      setValue('phone', profile.phone)
+      setValue('address', profile.address)
+      setValue('avatar', profile.avatar)
+      setValue('date_of_birth', profile.date_of_birth ? new Date(profile.date_of_birth) : new Date(1990, 0, 1))
+    }
+  }, [profile, setValue])
+
+  const onSubmit = handleSubmit(async (data) => {
+    console.log(data)
+    // await updateProfileMutation.mutateAsync({})
+  })
+
+  const value = watch()
+  console.log(value, errors)
+
   return (
     <>
       <div className='bg-white rounded-sm px-7 pb-20 shadow'>
@@ -10,55 +69,84 @@ const Profile = () => {
           <div className='mt-1 text-sm text-gray-700'>Manage profile information for account security</div>
         </div>
 
-        <div className='mt-8 flex flex-col-reverse md:flex-row md:items-start'>
-          <form className='mt-6 flex-grow md:mt-0 md:pr-12'>
+        <form className='mt-8 flex flex-col-reverse md:flex-row md:items-start' onSubmit={onSubmit}>
+          <div className='mt-6 flex-grow md:mt-0 md:pr-12'>
             <div className='flex flex-col flex-wrap sm:flex-row'>
               <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Email</div>
               <div className='sm:w-[80%] sm:pl-5'>
-                <div className='pt-3 text-gray-700'>pv*****@gmail.com</div>
+                <div className='pt-3 text-gray-700'>{profile?.email}</div>
               </div>
             </div>
 
             <div className='mt-6 flex flex-col flex-wrap sm:flex-row'>
               <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Name</div>
               <div className='sm:w-[80%] sm:pl-5'>
-                <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+                <Input
+                  classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                  register={register}
+                  name='name'
+                  placeholder='Name'
+                  errorMessage={errors.name?.message}
+                />
               </div>
             </div>
 
             <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
               <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Phone</div>
               <div className='sm:w-[80%] sm:pl-5'>
-                <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+                <Controller
+                  control={control}
+                  name='phone'
+                  render={({ field }) => (
+                    <InputNumber
+                      classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                      placeholder='Phone'
+                      errorMessage={errors.phone?.message}
+                      {...field}
+                      onChange={field.onChange}
+                    />
+                  )}
+                />
               </div>
             </div>
 
             <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
-              <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Email</div>
+              <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Address</div>
               <div className='sm:w-[80%] sm:pl-5'>
-                <Input classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm' />
+                <Input
+                  classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
+                  register={register}
+                  name='address'
+                  placeholder='Address'
+                  errorMessage={errors.address?.message}
+                />
               </div>
             </div>
+
+            <Controller
+              control={control}
+              name='date_of_birth'
+              render={({ field }) => (
+                <DateSelect
+                  errorMessage={errors.date_of_birth?.message}
+                  value={field.value}
+                  onChange={field.onChange}
+                />
+              )}
+            />
 
             <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
-              <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Date of birth</div>
+              <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'></div>
               <div className='sm:w-[80%] sm:pl-5'>
-                <div className='flex justify-between'>
-                  <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
-                    <option disabled>Day</option>
-                  </select>
-
-                  <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
-                    <option disabled>Month</option>
-                  </select>
-
-                  <select className='h-10 w-[32%] rounded-sm border border-black/10 px-3'>
-                    <option disabled>Year</option>
-                  </select>
-                </div>
+                <Button
+                  type='submit'
+                  className='flex items-center h-9 bg-orange px-5 text-center text-sm text-white hover:bg-orange/80'
+                >
+                  Save
+                </Button>
               </div>
             </div>
-          </form>
+          </div>
 
           <div className='flex justify-center md:w-72 md:border-l md:border-l-gray-200'>
             <div className='flex flex-col items-center'>
@@ -68,7 +156,10 @@ const Profile = () => {
 
               <input type='file' accept='.jpg,.jpeg,.png' className='hidden' />
 
-              <button className='flex h-10 items-center justify-end rounded-sm border bg-white px-6 text-sm text-gray-600 shadow-sm'>
+              <button
+                type='button'
+                className='flex h-10 items-center justify-end rounded-sm border bg-white px-6 text-sm text-gray-600 shadow-sm'
+              >
                 Change avatar
               </button>
 
@@ -78,7 +169,7 @@ const Profile = () => {
               </div>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </>
   )
