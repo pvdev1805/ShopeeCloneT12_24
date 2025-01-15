99fc511bdbebadca4558794bb47d1b57e96d575a
Phu Vo <u1153426@umail.usq.edu.au>
2025-01-15 19:08:51 +1000

    Implement 404 Page Not Found for invalid URLs

--- src/useRouteElements.tsx
+++ src/useRouteElements.tsx
@@ -14,6 +14,7 @@ import UserLayout from './pages/User/layouts/UserLayout'
 import ChangePassword from './pages/User/pages/ChangePassword'
 import Profile from './pages/User/pages/Profile'
 import HistoryPurchase from './pages/User/pages/HistoryPurchase'
+import NotFound from './pages/NotFound'
 
 const ProtectedRoute = () => {
   const { isAuthenticated } = useContext(AppContext)
@@ -99,6 +100,14 @@ const useRouteElements = () => {
       element: (
         <MainLayout>
           <ProductList />
+        </MainLayout>
+      )
+    },
+    {
+      path: '*',
+      element: (
+        <MainLayout>
+          <NotFound />
         </MainLayout>
       )
     }
